2d91abe9fafec5bd19f28e71197b99136d27673a
Andrew Verbitsky <averbitskiy5@gmail.com>
2019-10-27 17:44:36 +0200

    add info sunrise and sunset to app

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
 	$('#submit-weather').click(function(){
-		let city = $('#city').val();
+		let city = $('#city').val();		
 	
 	if(city != '') {
 
 		$.ajax('http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + 
-				 "&APPID=a8eb3bb7297afc2d6356acc7f8cff8fe").done(function(resp) {
-					 let temp = resp.weather.main;
-					 console.log(resp.timezone)
-				 })
-
+			"&APPID=a8eb3bb7297afc2d6356acc7f8cff8fe").done(function(resp) {
+				const temp = resp.main.temp;
+				const timeSunrise = new Date(resp.sys.sunrise * 1000);
+				const timeSunset = new Date(resp.sys.sunset * 1000);
+				const sunrise_Hour = timeSunrise.getHours() + 'h';
+				const sunrise_Min = timeSunrise.getMinutes() + 'min';
+				const sunset_Hour = timeSunset.getHours() + 'h';
+				const sunset_Min = timeSunset.getMinutes() + 'min';
+				const sunriseMain = sunrise_Hour + ' ' + sunrise_Min;
+				const sunsetMain = sunset_Hour + ' ' + sunset_Min;				
+				$(".card-lviv .card-temp").text(temp);
+				$(".card-header").text(city);
+				$(".card-info-sunrise").text(sunriseMain);			 
+				$(".card-info-sunset").text(sunsetMain);			 
+			});
+				 
 	} else{
 			$('#error').html('Введите название города');
 		}
 	});
 });
+
 
